e2c6c9db672ee67bd89b499d9901823abbe1afd3
kerey <akzholtay.k@gmail.com>
2017-09-08 17:39:42 +0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty';
 
 @Component({
   selector: 'app-root',
@@ -7,34 +6,5 @@ import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	private errorMessage: any[] = [];
-	constructor (
-	  private toastyService: ToastyService,
-		private toastyConfig: ToastyConfig) {
-	  this.toastyConfig.theme = 'material';
-	}
-  addToast() {
-      // Just add default Toast with title only
-      this.toastyService.default('Hi there');
-      // Or create the instance of ToastOptions
-      var toastOptions:ToastOptions = {
-          title: 'My title',
-          msg: 'The message',
-          showClose: true,
-          timeout: 5000,
-          theme: 'default',
-          onAdd: (toast:ToastData) => {
-            console.log('Toast ' + toast.id + ' has been added!');
-          },
-          onRemove: function(toast:ToastData) {
-            console.log('Toast ' + toast.id + ' has been removed!');
-          }
-      };
-      // Add see all possible types in one shot
-      this.toastyService.info(toastOptions);
-      this.toastyService.success(toastOptions);
-      this.toastyService.wait(toastOptions);
-      this.toastyService.error(toastOptions);
-      this.toastyService.warning(toastOptions);
-  }
+
 }
