548be353236065b67e83a3cbe9f98a7d63acd49b
kerey <akzholtay.k@gmail.com>
2017-11-06 13:14:11 +0600

    proposals infinity scroll added

--- src/app/common/config/server.ts
+++ src/app/common/config/server.ts
@@ -1,5 +1,5 @@
-// export const serverURL: string = 'https://api.dukender.kz';
-export const serverURL: string = 'https://api.dukender.org.kz';
+export const serverURL: string = 'https://api.dukender.kz';
+// export const serverURL: string = 'https://api.dukender.org.kz';
 //'http://54.144.117.231';
 // 'https://api.dukender.kz';
 // 'https://api.dukender.org.kz';
