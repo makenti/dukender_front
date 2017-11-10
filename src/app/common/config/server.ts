51e4d327dbe32dbdaa03ea3764651240f0b1bdce
kerey <akzholtay.k@gmail.com>
2017-11-10 11:34:00 +0600

    search proposals changed, request action changed

--- src/app/common/config/server.ts
+++ src/app/common/config/server.ts
@@ -1,6 +1,2 @@
 import { environment } from './../../../environments/environment';
 export const serverURL: string = environment.serverUrl;
-// export const serverURL: string = 'https://api.dukender.org.kz';
-//'http://54.144.117.231';
-// 'https://api.dukender.kz';
-// 'https://api.dukender.org.kz';
